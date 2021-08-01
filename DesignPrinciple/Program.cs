882991040917357b5c6ab68515782b93af38ec4c
ahamedsahib <adsahib39@gmail.com>
2021-08-01 20:12:00 +0530

    [Ahamed]Implemented using Liskov Substitution principle

--- DesignPrinciple/Program.cs
+++ DesignPrinciple/Program.cs
@@ -1,4 +1,6 @@
-﻿using DesignPrinciple.SingleResponsibilityPrinciple;
+﻿using DesignPrinciple.LiskovSubstitution;
+using DesignPrinciple.OpenClosedPrinciple;
+using DesignPrinciple.SingleResponsibilityPrinciple;
 using System;
 using System.IO;
 
@@ -11,18 +13,32 @@ namespace DesignPrinciple
             Console.WriteLine("Welcome!!!!");
             try
             {
-                string path = @"C:\Users\ahamedsahib.m\source\repos\DesignPrinciple\DesignPrinciple\SingleResponsibilityPrinciple\Sample.txt";
-                string fileContents = File.ReadAllText(path);
-                string[] arr = fileContents.Split(",");
-                Operation operation = new Operation();
-                foreach (string i in arr)
-                {
-                    operation.list.Add(i);
-                }
-                operation.AddEntry("JackFruit");
-                operation.RemoveItem("elephant");
-                operation.SaveToFile(path);
-                Console.WriteLine("Successfully Updated");
+                /* string path = @"C:\Users\ahamedsahib.m\source\repos\DesignPrinciple\DesignPrinciple\SingleResponsibilityPrinciple\Sample.txt";
+                 string fileContents = File.ReadAllText(path);
+                 string[] arr = fileContents.Split(",");
+                 Operation operation = new Operation();
+                 foreach (string i in arr)
+                 {
+                     operation.list.Add(i);
+                 }
+                 operation.AddEntry("JackFruit");
+                 operation.RemoveItem("elephant");
+                 operation.SaveToFile(path);
+                 Console.WriteLine("Successfully Updated");*/
+
+                /*double salary;
+                JuniorDev jd = new JuniorDev();
+                salary = jd.CalculateSalary();
+                Console.WriteLine($" junior dev Salary = {salary}");
+                SeniorDev sd = new SeniorDev();
+                salary = sd.CalculateSalary();
+                Console.WriteLine($" Senior dev Salary = {salary}");*/
+
+                int[] arr = { 1, 2, 3, 4, 5, 6 };
+                Calculator sum = new SumCalculate(arr);
+                Console.WriteLine($"Sum of array = {sum.CalculateSum()}");
+                Calculator evenSum = new EvenSumCalculate(arr);
+                Console.WriteLine($"The sum of even num in arr = {evenSum.CalculateSum()}");
             }
             catch (Exception ex)
             {
