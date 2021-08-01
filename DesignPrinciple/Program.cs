fb428a1797d654ee27c2cd386a880477c123b9e4
ahamedsahib <adsahib39@gmail.com>
2021-08-01 19:21:49 +0530

    [Ahamed]Implemented using SR principle

--- DesignPrinciple/Program.cs
+++ DesignPrinciple/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DesignPrinciple.SingleResponsibilityPrinciple;
+using System;
+using System.IO;
 
 namespace DesignPrinciple
 {
@@ -6,7 +8,26 @@ namespace DesignPrinciple
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome!!!!");
+            try
+            {
+                string path = @"C:\Users\ahamedsahib.m\source\repos\DesignPrinciple\DesignPrinciple\SingleResponsibilityPrinciple\Sample.txt";
+                string fileContents = File.ReadAllText(path);
+                string[] arr = fileContents.Split(",");
+                Operation operation = new Operation();
+                foreach (string i in arr)
+                {
+                    operation.list.Add(i);
+                }
+                operation.AddEntry("JackFruit");
+                operation.RemoveItem("elephant");
+                operation.SaveToFile(path);
+                Console.WriteLine("Successfully Updated");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
---
+++ DesignPrinciple/SingleResponsibilityPrinciple/IReport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPrinciple.SingleResponsibilityPrinciple
+{
+   
+        public interface IReport
+        {
+            public void AddEntry(string iten);
+            public void RemoveItem(string item);
+        }
+
+        public interface IReportSaver
+        {
+            public void SaveToFile(string path);
+        }
+ 
+}
