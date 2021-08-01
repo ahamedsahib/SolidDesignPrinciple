f83e5e018af6bf837cb17c44961fe12f903b039f
AHAMED <68241019+ahamedsahib@users.noreply.github.com>
2021-08-01 20:03:45 +0530

    [Ahamed]Implemented using Open closed principle

--- DesignPrinciple/OpenClosedPrinciple/SalaryCalulator.cs
+++ DesignPrinciple/OpenClosedPrinciple/SalaryCalulator.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace DesignPrinciple.OpenClosedPrinciple
 {
-    class SalaryCalulator
+   public abstract class SalaryCalculator
     {
+        public abstract double CalculateSalary();
+    }
+
+    public class SeniorDev : SalaryCalculator
+    {
+        public override double CalculateSalary()
+        {
+            double bp=10000, hra=2000, sb=800,totalSalary;
+            totalSalary = bp + hra + sb;
+            return totalSalary;
+        }
+    }
+    public class JuniorDev : SalaryCalculator
+    {
+        public override double CalculateSalary()
+        {
+            double bp = 5000, hra = 1000, sb = 300, totalSalary;
+            totalSalary = bp + hra + sb;
+            return totalSalary;
+        }
     }
 }
